f283b1afc36203f691e79da78d4d03790c73d255
Ashraf Nazar <ashraf.caspian@gmail.com>
2020-12-29 12:04:28 +0000

    Reduce code on Countries component

--- part2/country-data/src/components/Countries.js
+++ part2/country-data/src/components/Countries.js
@@ -1,19 +1,20 @@
 import Country from './Country'
 
-const Countries = ({ countries }) => {
+const Countries = ({ countries, showCountry }) => {
   const excessCountries = countries.length > 10
   const multipleCountries = countries.length > 0 && countries.length <= 10
   const singleCountry = countries.length === 1
 
   return (
     <>
-      {excessCountries && (
-        <p>Too many matches, please specify another filter</p>
-      )}
+      {excessCountries && <p>Too many matches, please specify another filter</p>}
       {multipleCountries && (
         <ul>
           {countries.map(country => (
-            <li key={country.name}>{country.name}</li>
+            <div key={country.name}>
+              <li key={country.alpha2Code}>{country.name}</li>
+              <button key={country.alpha3Code} onClick={showCountry} id={country.name}>Show country</button>
+            </div>
           ))}
         </ul>
       )}
