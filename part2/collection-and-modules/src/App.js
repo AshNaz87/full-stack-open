d922101c15e3298cf0017ab9f9a43c6013059911
Ashraf Nazar <ashraf.caspian@gmail.com>
2021-01-03 19:03:41 +0000

    Style collections and modules project

--- part2/collection-and-modules/src/App.js
+++ part2/collection-and-modules/src/App.js
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react'
 
 import Note from './components/Note'
+import Notification from './components/Notification'
+import Footer from './components/Footer'
 import noteService from './services/notes'
+
+import './index.css'
 
 const App = props => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('Some error occured...')
 
   useEffect(() => {
     noteService
       .getAll()
       .then(initialNotes => setNotes(initialNotes))
-  })
+  }, [newNote])
 
   const addNote = event => {
     event.preventDefault()
@@ -44,6 +49,8 @@ const App = props => {
       .then(returnedNote => setNotes(notes.map(note => note.id !== id ? note : returnedNote)))
       .catch(error => {        
         console.log(error)
+        setErrorMessage(`The note '${note.content}' was already removed from the server`)
+        setTimeout(() => setErrorMessage(null), 3000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -53,6 +60,7 @@ const App = props => {
   return (
     <>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>          
           Show {showAll ? 'important' : 'all'}
@@ -75,6 +83,7 @@ const App = props => {
         />
         <button type='submit'>Save</button>
       </form>
+      <Footer />
     </>
   )
 }
